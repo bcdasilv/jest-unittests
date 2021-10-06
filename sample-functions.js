c37573c758cea6911151e6741202b92f17eff630
Bruno da Silva <brunocarreiro@gmail.com>
2021-10-06 16:20:13 -0700

    Updating files after class

--- sample-functions.js
+++ sample-functions.js
@@ -2,27 +2,23 @@ function sum(a, b){
     return a + b;
 }
 
-function div (a, b){
-    return a / b;
+exports.div = function(a, b) {
+    if (b === 0){
+        throw new Error('Div by zero.');
+    }
+    return a / b; //or just return this line and expect Infinity
 }
 
-function containsNumbers(text){
+exports.containsNumbers = ((text) => {
     for (let i = 0; i < text.length; i++) {
         if (!isNaN(text.charAt(i)))
             return true;
     }
     return false;
-}
+});
+
+//Also, note different ways to export functions.
 
 exports.sum = sum;
-exports.div = div;
-exports.containsNumbers = containsNumbers;
-
-
-
-// function div (a, b){
-//     if (b === 0){
-//         throw new Error('Div by zero not supported.');
-//     }
-//     return a / b;
-// }
+// exports.div = div;
+// exports.containsNumbers = containsNumbers;
